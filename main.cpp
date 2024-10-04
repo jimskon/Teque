586249407833acd53b350ae0cc442ec07b24eb5d
James Skön <skonjp@kenyon.edu>
2024-10-04 11:28:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ int main()
             teque.insert(middle,v);
             
             
-            //if (teque.size()%2) 
-            //    ++middle;
+            if (teque.size()%2) 
+                --middle;
         } else {                        // assume get
             auto it = teque.begin();
             advance(it, v);
