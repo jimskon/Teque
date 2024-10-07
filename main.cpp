59966bc273fbb416e9d3f240f23b79f4e6d340d4
James Skön <skonjp@kenyon.edu>
2024-10-07 07:58:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main()
 
     scanf("%d", &n);
     for (int i=0; i<n; i++) {
-        //display(teque);
+        display(teque);
         //printf(" S: %lu, M: %d\n",teque.size(),*middle);
         scanf("%s %d", str, &v);
         //printf("ADD %d, %lu\n",v,teque.size());
