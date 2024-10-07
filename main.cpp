6e0a2e2323a6f8e0caaa4937e1c316e20691973b
James Skön <skonjp@kenyon.edu>
2024-10-07 07:54:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,56 +3,42 @@
 #include <list>
 using namespace std;
 
-void display(list<int> l) {
+void display(deque<int> l) {
   for (auto x: l) {
     printf(" %d",x);
   }
 }
 
+void insert_middle(deque<int> teque, int n) {
+  auto it = numbers.begin();
+  advance(it, (teque.size()+1) / 2);
+  teque.insert(it,n);
+}
+  
+
 int main()
 {
     int n;
-    list<int> teque;
+    deque<int> teque;
     char str[20];
     int v;
-    list<int>::iterator middle=teque.end();
 
     scanf("%d", &n);
-    //cin >> n;
     for (int i=0; i<n; i++) {
-        display(teque);
-        printf(" S: %lu, M: %d\n",teque.size(),*middle);
-        scanf("%s %d", str, &v);
+        //display(teque);
+        //printf(" S: %lu, M: %d\n",teque.size(),*middle);
+        //scanf("%s %d", str, &v);
         //printf("ADD %d, %lu\n",v,teque.size());
-        //cin >> c >> v;
         if (strcmp(str,"push_back")==0) {
             teque.push_back(v);
-            if (teque.size()==2) ++middle;
-            if (teque.size()%2==1) {
-                ++middle;
-            }
         } else if (strcmp(str,"push_front")==0) {
             teque.push_front(v);
-            if (teque.size()%2) 
-               --middle;
         } else if (strcmp(str,"push_middle")==0) {
-            //auto it = teque.begin();
-            //cout << "I:" << teque.size()+1 / 2 << endl;
-            //advance(it, (teque.size()+1) / 2); // Move iterator to the middle
-            //printf("M1: %d \n",*middle);
-            teque.insert(middle,v);
-            //printf("M2: %d \n",*middle);
-            
-            if (teque.size()%2==1) 
-              --middle;
-            else
-              --middle;
-            //printf("M3: %d \n",*middle);
+            insert_middle(teque,v)
         } else {                        // assume get
             auto it = teque.begin();
             advance(it, v);
             printf("%d\n",*it);
-            //cout << *it << endl;
         }
 
     }
